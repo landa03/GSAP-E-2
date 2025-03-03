e621f9767f8167097db3e5a5c175b5b25345a4aa
Omar Landa <omarlandita03@gmail.com>
2025-03-03 15:06:42 -0700

    omaigagit status! is donegit status!

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,107 @@ import { useState, useRef} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Paralax from './Paralax'
 
 import gsap from "gsap"
 import { useGSAP } from '@gsap/react';
 import { Timeline } from 'gsap/gsap-core';
+// import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger"; 
 
 
 function App() {
+  gsap.registerPlugin(useGSAP);
+  gsap.registerPlugin(ScrollTrigger);
+
   const [count, setCount] = useState(0)
-  let scrollable1 = useRef();
-  let timeline1 = new.Timeline();
+
   let scrollable1Ref = useRef();
-  
+  let scrollable2Ref = useRef();
+  let scrollable3Ref = useRef();
+
+  let timeLine1 = new Timeline();
+  let timeLine2 = new Timeline();
+  let timeLine3 = new Timeline();
+
+
   useGSAP(() => {
+    // ScrollTrigger: {}
+    timeLine1.from(scrollable1Ref.current,{
+      // y: 200, 
+      x: -2000, 
+      opacity: 0
+    });
     timeLine1.to(scrollable1Ref.current,{
-      x: promoSegmentWidth, 
-      duration: 3,
-      // delay: 3,
-    }
-  );
+      scrollTrigger:{
+        trigger: ".scrollable-1",
+        start: "top+=-200 bottom",
+        end: "top center",
+        // markers: true,
+        scrub: 1,
+      },
+      // y: 0, 
+      x: 0,
+      duration: 2,
+      opacity: 1
+    });
+
+
+    timeLine2.from(scrollable2Ref.current,{
+      y: 200, 
+      opacity: 0
+    });
+    timeLine2.to(scrollable2Ref.current,{
+      scrollTrigger:{
+        trigger: ".scrollable-2",
+        start: "top+=-200 bottom",
+        end: "top center",
+        // markers: true,
+        scrub: 1,
+      },
+      y: 0, 
+      // x: -200,
+      duration: 2,
+      opacity: 1
+    });
+
+
+    timeLine3.from(scrollable3Ref.current,{
+      // y: 200, 
+      opacity: 0
+    });
+    timeLine3.to(scrollable3Ref.current,{
+      scrollTrigger:{
+        trigger: ".scrollable-3",
+        start: "top bottom",
+        end: "top center",
+        // markers: true,
+        scrub: 1,
+      },
+      // y: 0, 
+      // x: -200,
+      duration: 2,
+      opacity: 1
+    });
   });
 
   return (
     <>
-      <div className='scrollable-1'>
+      <div id='dooky'>
+        top page dooky
+      </div>
+      <div className='scrollable-1' ref={scrollable1Ref}>
         soy un segmento escroleable, soy el numero 1
       </div>
-      <div className='scrollable-2'>
+      <div className='scrollable-2' ref={scrollable2Ref}>
         soy un segmento escroleable, soy el numero 2
       </div>
-      <div className='scrollable-3'>
+      <div className='scrollable-3' ref={scrollable3Ref}>
         soy un segmento escroleable, soy el numero 3
+      </div>
+      <Paralax/>
+      <div id='dooky'>
+        top page dooky
       </div>
     </>
   )
---
+++ src/Paralax.jsx
@@ -0,0 +1,89 @@
+import { useState, useRef} from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import gsap from "gsap"
+import { useGSAP } from '@gsap/react';
+import { Timeline } from 'gsap/gsap-core';
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger"; 
+
+export default function Paralax() {
+    gsap.registerPlugin(useGSAP);
+    gsap.registerPlugin(ScrollTrigger);
+    let paralaxRef = useRef();
+    let timeLine1 = new Timeline();
+
+    let paralaxContStyle = {
+        width: "100%",
+        // display: "flex",
+        // flexDirection: "column",
+        height: "180px",
+        overflow: "hidden"
+      }
+    let paralaxBoyStyle = {
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "360px",
+        // overflow: "hidden"
+      }
+      let paralaxStyle1 = {
+        width: "100%",
+        backgroundColor: "red",
+        minHeight: "30px"
+      }
+      let paralaxStyle2 = {
+        width: "100%",
+        backgroundColor: "green",
+        minHeight: "30px"
+      }
+      let paralaxStyle3 = {
+        width: "100%",
+        backgroundColor: "blue",
+        minHeight: "30px"
+      }
+
+    useGSAP(() => {
+        timeLine1.from(paralaxRef.current,{
+            y: "-50%"
+        });
+        timeLine1.to(paralaxRef.current,{
+            scrollTrigger:{
+                trigger: ".paralax-boy",
+                // trigger: paralaxRef.current,
+                start: "top bottom",
+                end: "bottom top",
+                scrub: true,
+                markers: true,
+            },
+            y: "0%",
+            duration: 1,
+            ease: "none"
+        });
+    });
+
+    return(
+        <>
+            <div style={paralaxContStyle}>
+                <div className='paralax-boy' style={paralaxBoyStyle} ref={paralaxRef}>
+                    <div style={paralaxStyle1}></div>
+                    <div style={paralaxStyle2}></div>
+                    <div style={paralaxStyle3}></div>
+                    <div style={paralaxStyle1}></div>
+                    <div style={paralaxStyle2}></div>
+                    <div style={paralaxStyle3}></div>
+                    <div style={paralaxStyle1}></div>
+                    <div style={paralaxStyle2}></div>
+                    <div style={paralaxStyle3}></div>
+                    <div style={paralaxStyle1}></div>
+                    <div style={paralaxStyle2}></div>
+                    <div style={paralaxStyle3}></div>
+                </div>
+            </div>
+        </>
+    )
+
+
+}
